30a8bb329023fc0d3275cdc6759bc7da18a0d908
Eragon1731 <cye@ucsd.edu>
2017-06-05 16:10:37 -0700

    Add files via upload

---
+++ GameController.cpp
@@ -0,0 +1,80 @@
+/////////////////////
+// Controller.cpp
+/////////////////////
+
+#include "Controller.h"
+
+/////////////////////////////////////////////////////
+
+Controller::Controller() {
+
+	// Creates the model for the controller
+	Model conM (pathToController);
+	controllerModel = conM;
+
+	// Sets the position / rotation / scale
+	position = glm::vec3(0, 0, 0);
+}
+
+void Controller::loadS() {
+	// Create the shader to use for the controller
+	GLint conS = LoadShaders(vertexShaderPath, fragShaderPath);
+	controllerShader = conS;
+}
+
+glm::mat4 Controller::GetModelMatrix() {
+	glm::mat4 modelMat = glm::translate(position);
+	return modelMat;
+}
+
+glm::vec3 Controller::GetColor() {
+	return laser.color;
+}
+
+void Controller::Render(glm::mat4 view, glm::mat4 proj) {
+
+	glUseProgram(controllerShader);
+
+	// Calculate the toWorld matrix for the model
+	glm::mat4 model;
+	model = glm::translate(model, position);
+
+	glm::quat orientation = glm::quat(rotation.w, rotation.x, rotation.y, rotation.z);
+	glm::mat4 rotationMatrix = glm::toMat4(orientation);
+	model *= rotationMatrix;
+	model = glm::rotate(model, glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+
+	model = glm::scale(model, glm::vec3(0.05f, 0.05f, 0.05f));
+	glUniformMatrix4fv(glGetUniformLocation(controllerShader, "model"), 1, GL_FALSE, &model[0][0]);
+	glUniformMatrix4fv(glGetUniformLocation(controllerShader, "view"), 1, GL_FALSE, &view[0][0]);
+	glUniformMatrix4fv(glGetUniformLocation(controllerShader, "projection"), 1, GL_FALSE, &proj[0][0]);
+
+	controllerModel.Draw(controllerShader);
+
+	// Get the button presses
+	if (inputState.Buttons & btn1) {
+		// on button 1 press
+	}else {
+		
+	}
+
+	// Get the trigger presses
+	if (inputState.IndexTrigger[hand] > .5) {
+		laser.SetRed();
+	}
+	else {
+		//laser.SetGreen();
+	}
+
+	// Render the laser
+	laser.position = position;
+	laser.rotation = rotation;
+	laser.Render(view, proj);
+
+	// Calculates the ray equation
+	ray.origin = position;
+	ray.dir = glm::normalize(orientation * glm::vec3(0, 0, -1));
+	ray.dist = 75.0f; //magic number - same as laserDist in Laser.h
+
+}
+
---
+++ GameController.h
@@ -0,0 +1,73 @@
+/////////////////////
+// Controller.h
+/////////////////////
+
+#pragma once
+#include <Windows.h>
+#include "Shader.h"
+#include "Model.h"
+#include "Laser.h"
+
+// Std includes
+#include <stdio.h>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <vector>
+
+// GLM includes
+#include <glm/glm.hpp>
+#include <glm/gtc/noise.hpp>
+#include <glm/gtx/rotate_vector.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/quaternion.hpp>
+#include <glm/gtx/quaternion.hpp>
+#include <GL/glew.h>
+
+//#include <OVR_CAPI.h>
+//#include <OVR_CAPI_GL.h>
+//
+#include "LEAP_SDK\include\Leap.h"
+
+using namespace Leap; 
+
+struct Ray {
+	glm::vec3 origin;
+	glm::vec3 dir;
+	float dist;
+};
+
+class Controller {
+public:
+	glm::vec3 position;
+	glm::vec4 rotation;
+	glm::vec3 scale;
+
+	// Controlls for this controller
+	//ovrInputState inputState;
+	//ovrTouch btn1, btn2;
+	//ovrHandType hand;
+
+	Controller();
+	void loadS(); 
+	void Render(glm::mat4 view, glm::mat4 proj);
+
+	glm::vec3 GetColor();
+
+	Ray ray;
+private:
+	/* Data */
+	Laser laser;
+	Model controllerModel;
+	GLchar* pathToController = "H:/FinalProject/MinimalVR-master/objects/cube.obj";
+	GLchar* vertexShaderPath = "H:/FinalProject/MinimalVR-master/Minimal/shader_1.vert";
+	GLchar* fragShaderPath   = "H:/FinalProject/MinimalVR-master/Minimal/shader_1.frag";
+	GLint controllerShader;
+
+
+	/* Functions */
+	glm::mat4 GetModelMatrix();
+};
